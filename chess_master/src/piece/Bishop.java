e0a4f59d94b898b8814ae09c4feea1c96cebdda0
Gabriel Santana Fernandes <64505292+Gabriel-sFernandes@users.noreply.github.com>
2024-04-30 18:09:27 -0300

    Add files via upload

--- chess_master/src/piece/Bishop.java
+++ chess_master/src/piece/Bishop.java
@@ -6,6 +6,8 @@ public class Bishop extends Piece{
 
 	public Bishop(int color, int col, int row) {
 		super(color, col, row);
+		
+		type = Type.BISHOP;
 	
 		if(color == GamePanel.WHITE) {
 			image = getImage("/piece/w_bishop_1x_ns");
@@ -14,6 +16,19 @@ public class Bishop extends Piece{
 			image = getImage("/piece/b_bishop_1x_ns");
 		}
 
+	}
+	
+	public boolean canMove(int targetCol, int targetRow) {
+
+		if (isWithinBoard(targetCol, targetRow) && isSameSquare(targetCol, targetRow) == false) {
+			if(Math.abs(targetCol - preCol) == Math.abs(targetRow - preRow)) {
+				if(isValidSquare(targetCol, targetRow)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 }
--- chess_master/src/piece/Queen.java
+++ chess_master/src/piece/Queen.java
@@ -2,18 +2,39 @@ package piece;
 
 import main.GamePanel;
 
-public class Queen extends Piece{
+public class Queen extends Piece {
 
 	public Queen(int color, int col, int row) {
 		super(color, col, row);
 		
-		if(color == GamePanel.WHITE) {
+		type = Type.QUEEN;
+
+		if (color == GamePanel.WHITE) {
 			image = getImage("/piece/w_queen_1x_ns");
-		}
-		else {
+		} else {
 			image = getImage("/piece/b_queen_1x_ns");
 		}
 
+	}
+
+	public boolean canMove(int targetCol, int targetRow) {
+		if (isWithinBoard(targetCol, targetRow) && isSameSquare(targetCol, targetRow) == false) {
+			
+			//vertical and horizontal
+			if(targetCol == preCol || targetRow == preRow) {
+				if(isValidSquare(targetCol, targetRow) && pieceIsOnStreightLine(targetCol, targetRow) == false) {
+					return true;
+				}
+			}
+			
+			//diagonal
+			if(Math.abs(targetCol - preCol) == Math.abs(targetRow) - preRow) {
+				if(isValidSquare(targetCol, targetRow) && pieceIsOnDiagonalLine(targetCol, targetRow) == false) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
