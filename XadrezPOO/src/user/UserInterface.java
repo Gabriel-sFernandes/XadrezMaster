901a5fa4123c7c05277923520add93613182f44f
Gabriel Santana Fernandes <64505292+Gabriel-sFernandes@users.noreply.github.com>
2024-05-06 10:11:15 -0300

    Delete XadrezPOO directory

--- XadrezPOO/src/user/UserInterface.java
+++
@@ -1,8 +0,0 @@
-package user;
-
-public interface UserInterface {
-	public void addUser(Long id, String username, String password);
-	public void removeUser(Long id);
-	public boolean isValidLogin(String nickname, String password);
-	public void updateUserPassword(Long id, String newPassword);
-}
